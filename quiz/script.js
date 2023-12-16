2903cf3b33d6b5c925c5344bad47aade62b824f3
EmiliyaShtereva <123276538+EmiliyaShtereva@users.noreply.github.com>
2023-12-16 13:21:04 +0200

    renamed quiz file to script and added example questions

---
+++ quiz/script.js
@@ -0,0 +1,31 @@
+const questions = [
+    {
+        question: 'Which is the largest animal in the world?',
+        answer: [
+            {text: 'Shark', correct: false},
+            {text: 'Blue whale', correct: true},
+            {text: 'Elephant', correct: false},
+            {text: 'Giraffe', correct: false},
+        ],
+        explanation: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae consequatur adipisci illum reprehenderit quod impedit harum, accusamus at quis ad minima asperiores optio consequuntur libero voluptate ipsa neque dolorem itaque?'
+    }, 
+    {
+        question: 'Which is the smallest continent in the world?',
+        answer: [
+            {text: 'Asia', correct: false},
+            {text: 'Australia', correct: true},
+            {text: 'Arctic', correct: false},
+            {text: 'Africa', correct: false},
+        ],
+        explanation: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae consequatur adipisci illum reprehenderit quod impedit harum, accusamus at quis ad minima asperiores optio consequuntur libero voluptate ipsa neque dolorem itaque?'
+    }, {
+        question: 'Which is the smallest countryl in the world?',
+        answer: [
+            {text: 'Vatican City', correct: true},
+            {text: 'Bhutan', correct: true},
+            {text: 'Nepal', correct: false},
+            {text: 'Shri Lanka', correct: false},
+        ],
+        explanation: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae consequatur adipisci illum reprehenderit quod impedit harum, accusamus at quis ad minima asperiores optio consequuntur libero voluptate ipsa neque dolorem itaque?'
+    }
+]
